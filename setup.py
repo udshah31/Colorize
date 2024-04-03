1d756fe12f4665940070e495313bc484e95cbf69
udshah31 <udshah31@g.gmail.com>
2024-04-03 10:19:05 -0500

    Updated

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def get_requirements():
 
 
 setup(
-    name="DeOldify",
+    name="Colorize",
     version="0.0.1",
     packages=find_packages(exclude=["tests"]),
     url="",
@@ -25,15 +25,7 @@ setup(
     # long_description=get_long_description(),
     # long_description_content_type="text/markdown",
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Framework :: Jupyter",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+        
     ],
     install_requires=get_requirements(),
     python_requires=">=3.6",
